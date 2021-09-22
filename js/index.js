b624ab751d88925d048bed18e90107ff8105cbae
Yang guang <ajjergw14isfcu@gmail.com>
2021-09-23 00:50:06 +0900

    feat：2.0

--- js/index.js
+++ js/index.js
@@ -1,33 +1,36 @@
-var oCalculator = document.getElementsByClassName('J_calculator')[0],
-    oResult = oCalculator.getElementsByClassName('result')[0],
-    fInput = oCalculator.getElementsByTagName('input')[0],
-    sInput = oCalculator.getElementsByTagName('input')[1],
-    oBtnGroup = oCalculator.getElementsByClassName('btn-group')[0];
-    oBtnGroup.addEventListener('click', onBtnClick, false);
+// 立即执行函数，独立作用域，控制全局变量
+;(function (doc, initTools, initCompute) {
+    var oCalculator = doc.getElementsByClassName('J_calculator')[0],
+        oResult = oCalculator.getElementsByClassName('result')[0],
+        oInputs = oCalculator.getElementsByTagName('input'),
+        oBtnGroup = oCalculator.getElementsByClassName('btn-group')[0];
+        oBtnGroup.addEventListener('click', onBtnClick, false);
+    
+    // 初始化操作
+    var init = function () {
+        bindEvent();
+    }
+    
+    function bindEvent() {
+        oBtnGroup.addEventListener('click', onBtnClick, false);
+    }
 
-function onBtnClick(ev) {
-    var e = ev || window.event,
-        tar = e.target,
-        tagName = tar.tagName.toLowerCase();
-    if (tagName === 'button') {
-        var method = tar.getAttribute('data-method'),
-            fVal = Number(fInput.value.replace(/\s+/g, '')) || 0,
-            sVal = Number(sInput.value.replace(/\s+/g, '')) || 0;
-        switch (method) {
-            case 'plus':
-                oResult.innerHTML = fVal + sVal;
-                break;
-            case 'minus':
-                oResult.innerHTML = fVal - sVal;
-                break;
-            case 'mul':
-                oResult.innerHTML = fVal * sVal;
-                break;
-            case 'div':
-                oResult.innerHTML = fVal / sVal;
-                break;
-            default:
-                break;
+    // 输出结果
+    function renderResult(result) {
+        oResult.innerHTML = result;
+    }
+
+    // 按钮点击事件处理
+    function onBtnClick(ev) {
+        var tar = initTools.getTarget(ev),
+            tagName = tar.tagName.toLowerCase();
+        if (tagName === 'button') {
+            var method = tar.getAttribute('data-method'),
+                fVal = initTools.digitalize(oInputs[0].value),
+                sVal = initTools.digitalize(oInputs[1].value);
+            renderResult(initCompute[method](fVal, sVal));
         }
     }
-}
+
+    init();
+})(document, initTools, initCompute)
